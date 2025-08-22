9b5c761de9d6fa394efac526f7e1960d3fecd9bf
unknown00p <rajpoint8976@gmail.com>
2025-08-22 20:47:28 +0530

    some minor changes

--- new-frontend/src/page/Home.tsx
+++ new-frontend/src/page/Home.tsx
@@ -110,7 +110,7 @@ function Home() {
         <CategoryCarousel />
       </div> */}
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {videos.map((video) => (
           <div
             key={video.id}
